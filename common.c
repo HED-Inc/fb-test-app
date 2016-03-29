ef9c21a2e55160ad52c40f7c2a07fcab95e484e1
Matthew Starr <mstarr@hedonline.com>
2016-03-29 14:11:41 -0500

    Remove KDSETMODE ioctl call
    This call looks to not be needed and does not switch the virtual
    terminal back to KD_TEXT mode after exiting causing some issues for
    other applications.

--- common.c
+++ common.c
@@ -38,12 +38,7 @@
 void fb_open(int fb_num, struct fb_info *fb_info)
 {
 	char str[64];
-	int fd,tty;
-
-	tty = open("/dev/tty1", O_RDWR);
-
-	if(ioctl(tty, KDSETMODE, KD_GRAPHICS) == -1)
-		printf("Failed to set graphics mode on tty1\n");
+	int fd;
 
 	sprintf(str, "/dev/fb%d", fb_num);
 	fd = open(str, O_RDWR);
